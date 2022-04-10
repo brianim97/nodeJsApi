840d9b7870b94e08b0cba78725d38a3694400204
pointH3 <joaquinbastias321@gmail.com>
2022-04-10 19:17:46 -0300

    Arreglos de parte del server

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -4,12 +4,14 @@ const btnInput = document.getElementById("btnInput")
 window.onload=()=>{
     var nameArray = ["Gideon","Phillip","Elon"];
     var rand1 = Math.floor(Math.random()*nameArray.length);
-
     var surnameArray = ["McKenzie","Marmaduke","Dominic","Gated"];
     var rand2 = Math.floor(Math.random()*surnameArray.length);
-
-
     var res = nameArray[rand1]+" "+surnameArray[rand2];
+    nameInput.placeholder=res;
+};
 
-    nameInput.placeholder=res
+const searchUser = async ()=>{
+    const url = "https://pointh3.herokuapp.com/api/users/";
+    const users = await fetch(url).then(el=>el.json()).then(el=>{return el.users});
+    console.log(users)
 };
