ebe9b63fe76086c0fc13919538c24a2d42dd409c
pointH3 <joaquinbastias321@gmail.com>
2022-04-11 00:27:59 -0300

    .

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,6 +1,6 @@
 const nameInput = document.getElementById("nameInput");
 const btnInput = document.getElementById("btnInput")
-
+const container = document.getElementById("container")
 window.onload=()=>{
     var nameArray = ["Gideon","Phillip","Elon"];
     var rand1 = Math.floor(Math.random()*nameArray.length);
@@ -10,13 +10,20 @@ window.onload=()=>{
     nameInput.placeholder=res;
 };
 
+//<li class="list-group-item">A second item <span class="text-primary">HOLA</span> </li>     
+
 const searchUser = async ()=>{
+    container.innerHTML=" ";
     const url = "https://pointh3.herokuapp.com/api/users";
-    const users = await fetch(url,{
-        method:"POST",
-        body:JSON.stringify({
-            name:"Joaquin"
-        })
-    }).then(el=>el.json());
-    console.log(users)
+    const users = await fetch(url).then(el=>el.json()).then(el=>{return el.users});
+    const a = [1,2,3,4,"asd"]
+    users.forEach(el => {
+        var obj = document.createElement("li");
+        obj.classList.add("list-group-item");
+        obj.innerHTML = `${el.name} <span class="text-primary">ID: ${el.password}</span> </li>`;
+        container.appendChild(obj);
+        console.log(el)
+    });
+    
 };
+
