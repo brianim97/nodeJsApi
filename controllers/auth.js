ef420eb5da9e8e7ab254d9d32abd13daf5d3d513
pointH3 <joaquinbastias321@gmail.com>
2022-04-14 02:35:10 -0300

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,47 +1,45 @@
 const bcrypt = require("bcryptjs")
 const { response } = require("express")
 const jwt = require("jsonwebtoken")
-//const { createJwt } = require("../helpers/createJwt")
+const { createJwt } = require("../helpers/createJwt")
 //const { googleVerify } = require("../helpers/google-verify")
 const User = require("../models/user")
 
-const login = async(req = require, res = response)=>{
-    const {mail,password} = req.body
+const login = async (req = require, res = response) => {
+    const { mail, password } = req.body
 
     try {
         //Verificar si el mail existe
-        const user = await User.findOne({mail})  
+        const user = await User.findOne({ mail })
 
-        if(!user){
+        if (!user) {
             return res.status(400).json({
-                msg:"Mail o Contraseña no son correctos - mail"
+                msg: "Mail o Contraseña no son correctos - mail"
             })
         }
 
         //Si el usuario esta activo
-        if(!user.status){
+        if (!user.status) {
             return res.status(400).json({
-                msg:"Usuario deshabilitado"
+                msg: "Usuario deshabilitado"
             })
         }
         //Verificar la contraseña 
-       const passwordVerify = bcrypt.compareSync(password, user.password) 
-       if(!passwordVerify){
-        return res.status(400).json({
-            msg:"Mail o Contraseña no son correctos - pass"
-        })
-    }
+        if (password != user.password) return res.status(400).json({ msg: "Mail o Contraseña no son correctos - pass" })
+
+
         //Generar el JWT
         const token = await createJwt(user.id);
+
         res.json({
             user,
-           token
+            token
         })
-        
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            msg:`Error: ${error}`
+            msg: `Error: ${error}`
         })
     }
 };
